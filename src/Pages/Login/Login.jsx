be30b87743e487ad8f6355c68f49e84d60ac38dc
Mahmud Munna <smbmunna@gmail.com>
2023-11-05 02:26:21 +0600

    AuthProvider and Custom Hook useAuth added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,35 @@
-
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            <h1>Login page</h1>            
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form className="card-body">
+                            <h2 className='text-2xl font-semibold text-blue-700 text-center'>Login</h2>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <Link to='/registration'>New to This Site? <span className='font-bold  text-blue-800'>Register</span></Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,8 +1,43 @@
+import { Link } from 'react-router-dom';
+import useAuth from '../../Hooks/useAuth';
+
 
 const Registration = () => {
+    const {user}= useAuth();
+
+    const handleRegistration = event => {
+        event.preventDefault();        
+        console.log(user, 'reg')
+    }
     return (
         <div>
-            <h1>Registration page</h1>            
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleRegistration} className="card-body">
+                            <h2 className='text-2xl font-semibold text-blue-700 text-center'>Register</h2>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <Link to='/login'>Already have Account? <span className='font-bold  text-blue-800'>Login</span></Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
