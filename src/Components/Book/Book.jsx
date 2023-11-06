18953f819fb3c4343e7e8f4495890792441cf4ae
Mahmud Munna <smbmunna@gmail.com>
2023-11-06 14:13:38 +0600

    Added book cards in the books page.

---
+++ src/Components/Book/Book.jsx
@@ -0,0 +1,22 @@
+
+const Book = ({book}) => {
+    const {_id, name, image, quantity, author, category, sdescription,rating}=book;
+    return (
+        <div>
+            <div className="card w-72 bg-base-100 shadow-xl">
+                {/* <figure className="px-10 pt-10">
+                    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                </figure> */}
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{name}</h2>
+                    <p>{sdescription}</p>
+                    <div className="card-actions">
+                        <button className="btn btn-primary">Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,10 +1,16 @@
 import { useLoaderData } from "react-router-dom";
+import Book from "../../Components/Book/Book";
 const Books = () => {
-    const books= useLoaderData();
-    console.log(books);
+    const books = useLoaderData();
+    const { category } = books;
     return (
         <div>
-            <h1>Books for Category: </h1>            
+            <h1>Books for Category: {category}</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                {
+                    books.map(book => <Book key={book._id} book={book} />)
+                }
+            </div>
         </div>
     );
 };
