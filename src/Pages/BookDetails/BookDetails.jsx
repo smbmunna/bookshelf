4231e7dafe4fd2c26504d677c4f08abde52748e2
Mahmud Munna <smbmunna@gmail.com>
2023-11-08 18:08:09 +0600

    Book details page updated

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -3,13 +3,14 @@ import useAuth from '../../Hooks/useAuth';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
+import StarRatings from 'react-star-ratings';
 const BookDetails = () => {
     const book = useLoaderData();
     //loading user from context
     const { user } = useAuth();
     const { _id, name, image, quantity, author, category, sdescription, rating } = book;
-    
-    
+
+
 
     //setting previous borrowed books in a state
     const [previousBorrowedBooks, setPreviousBorrowedBooks] = useState([]);
@@ -79,42 +80,47 @@ const BookDetails = () => {
             <div className="text-black">
                 <h1 className="text-3xl font-bold">{name}</h1>
                 <p className="mt-10">{ }</p>
-                <p><span className="font-bold mr-2">Announcement:</span> </p>
-                <p><span className="font-bold mr-2">Display:</span> </p>
-                <p><span className="font-bold mr-2">Operating System:</span> </p>
-                <p><span className="font-bold mr-2">RAM:</span> </p>
-                <p><span className="font-bold mr-2">Camera:</span></p>
-                <p><span className="font-bold mr-2">Battery:</span></p>
+                <p className="my-2 text-xl"><span className="font-bold text-xl mr-2">Author:</span>{author} </p>
+                <p className="my-2 text-xl"><span className="font-bold text-xl mr-2">Category:</span>{category} </p>
+                <StarRatings
+                    rating={rating}
+                    starDimension="20px"
+                    starSpacing="8px"
+                />
+                
+
+
+                <p className="my-2 text-xl"><span className="font-bold text-xl mr-2">Short Description:</span>{sdescription} </p>
                 <button
-                    className="btn btn-primary bg-[#2c2c2c91] rounded-none  mt-8"
+                    className="btn text-white rounded-none bg-orange-500 mt-8"
                     disabled={quantity == 0 ? true : false}
                     onClick={() => document?.getElementById('my_modal_5').showModal()}
-                    
+
                 >Borrow Book
                 </button>
-                    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-                        <div className="modal-box">
-                            <h3 className="font-bold text-lg">Please Enter the Following Information</h3>
-                            {/* <p className="py-4">Press ESC key or click the button below to close</p> */}
-                            <div className="modal-action">
-                                <form onSubmit={handleAddToCart} >
-                                    {/* if there is a button in form, it will close the modal */}
-                                    <div>
-                                        <input defaultValue={user?.displayName} className="input input-bordered" required type="text" name="name" placeholder="Full Name" />
-                                        <input defaultValue={user?.email} className="input input-bordered" required type="email" name="email" placeholder="Email" />
-                                        <label className="label">
-                                            <span className="label-text">Return Date</span>
-                                        </label>
-                                        <input className="input input-bordered" required type="date" name="return_date" placeholder="Return Date" />
-                                    </div>
-                                    <button
-                                        className="btn">Borrow Book</button>
-                                </form>
-                            </div>
+                <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">Please Enter the Following Information</h3>
+                        {/* <p className="py-4">Press ESC key or click the button below to close</p> */}
+                        <div className="modal-action">
+                            <form onSubmit={handleAddToCart} >
+                                {/* if there is a button in form, it will close the modal */}
+                                <div>
+                                    <input defaultValue={user?.displayName} className="input input-bordered" required type="text" name="name" placeholder="Full Name" />
+                                    <input defaultValue={user?.email} className="input input-bordered" required type="email" name="email" placeholder="Email" />
+                                    <label className="label">
+                                        <span className="label-text">Return Date</span>
+                                    </label>
+                                    <input className="input input-bordered" required type="date" name="return_date" placeholder="Return Date" />
+                                </div>
+                                <button
+                                    className=" my-8 btn text-white rounded-none bg-orange-500">Borrow Book</button>
+                            </form>
                         </div>
-                    </dialog>
+                    </div>
+                </dialog>
             </div>
-            <Link to={`/readBook/${_id}`} className="btn btn-primary bg-[#2c2c2c91] rounded-none mt-4 ">Read this Book</Link>
+            <Link to={`/readBook/${_id}`} className="btn text-white rounded-none bg-orange-500 mt-4 ">Read this Book</Link>
         </div >
     );
 };
