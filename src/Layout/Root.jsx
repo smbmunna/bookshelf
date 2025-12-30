d054c50836706e7c11dc59e9062a110612743a13
smbmunna <smbmunna@gmail.com>
2025-12-31 00:43:18 +0600

    Black bg changed

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -4,7 +4,7 @@ import Footer from '../Components/Footer/Footer';
 
 const Root = () => {
     return (
-        <div className="bg-[url('https://i.ibb.co/3ch67TH/symbol-scatter-haikei-1.png')] dark:bg-[url('https://i.ibb.co/BZ4z4xG/symbol-scatter-haikei-2.png')]">
+        <div className="bg-[url('https://i.ibb.co.com/qLnJH49R/bg-black.jpg')] dark:bg-[url('https://i.ibb.co/BZ4z4xG/symbol-scatter-haikei-2.png')]">
             <Navbar />
             <Outlet />
             <Footer />
